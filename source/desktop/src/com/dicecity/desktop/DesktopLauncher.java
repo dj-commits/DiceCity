e5a0d7fe76533c14028e7392d3405738e3e2d6df
prime <prime@DESKTOP-NC7HHT0>
2021-02-06 13:28:09 -0600

    initial commit

---
+++ source/desktop/src/com/dicecity/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.dicecity.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.dicecity.DiceCity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DiceCity(), config);
+	}
+}
